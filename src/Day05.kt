63d812a063c6b7eecc6e808803dea4885b6383e7
Kent <kent.yuan@Gmail.com>
2025-09-23 01:37:19 +0200

    Day 05 ✅

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,5 @@
 import utils.*
+import kotlin.math.abs
 
 // https://adventofcode.com/2018/day/5
 fun main() {
@@ -7,17 +8,31 @@ fun main() {
     val input = readInput(today)
     val testInput = readTestInput(today)
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>)= part1(input.single())
+    fun part2(input: List<String>)= part2(input.single())
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
-    chkTestInput(Part1, testInput, 0) { part1(it) }
+    chkTestInput(Part1, testInput, 10) { part1(it) }
     solve(Part1, input) { part1(it) }
 
-    chkTestInput(Part2, testInput, 0) { part2(it) }
+    chkTestInput(Part2, testInput, 4) { part2(it) }
     solve(Part2, input) { part2(it) }
+}
+
+
+private val offset = abs('A' - 'a')
+private fun part1(input: String): Int {
+    val stack = ArrayDeque<Char>()
+    input.forEach { c ->
+        if (stack.isNotEmpty() && abs(stack.last() - c) == offset) {
+            stack.removeLast()
+        } else {
+            stack.addLast(c)
+        }
+    }
+    return stack.size
+}
+
+private fun part2(input: String): Int {
+    return ('a'..'z').minOf { c -> part1(input.replace("$c", "", true)) }
+
 }
